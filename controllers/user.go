b6456f49c2ba9cf735b170ec34f1c5f81777dcd6
mardiniii <sezama03@hotmail.com>
2018-05-08 11:48:01 -0400

    Refactor create user method to don't duplicate records

--- controllers/user.go
+++ controllers/user.go
@@ -7,8 +7,8 @@ import (
 	"log"
 	"net/http"
 
-	dbs "../dbs"
-	models "../models"
+	dbs "github.com/Mardiniii/serapis_api/dbs"
+	models "github.com/Mardiniii/serapis_api/models"
 )
 
 // User routes handlers
@@ -20,18 +20,19 @@ func UserIndex(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewEncoder(w).Encode(dbs.UsersRepo)
 	if err != nil {
-		log.Fatal(err)
+		log.Panicln(err)
 	}
 }
 
 // UserCreate handler for user/ - POST
 func UserCreate(w http.ResponseWriter, r *http.Request) {
 	var user models.User
+	var err error
 
 	// Extract JSON payload
 	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
 	if err != nil {
-		log.Fatal(err)
+		log.Panicln(err)
 	}
 	defer r.Body.Close()
 
@@ -43,16 +44,24 @@ func UserCreate(w http.ResponseWriter, r *http.Request) {
 		err = json.NewEncoder(w).Encode("The user was not created")
 
 		if err != nil {
-			log.Fatal(err)
+			log.Panicln(err)
 		}
 	}
 
 	// Create the new user
-	u := dbs.RepoCreateUser(user)
 	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
-	w.WriteHeader(http.StatusCreated)
-	err = json.NewEncoder(w).Encode(u)
+	user, err = dbs.RepoCreateUser(user)
+
 	if err != nil {
-		log.Fatal(err)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusConflict)
+			return
+		}
+	} else {
+		w.WriteHeader(http.StatusCreated)
+		err = json.NewEncoder(w).Encode(user)
+		if err != nil {
+			log.Panicln(err)
+		}
 	}
 }
