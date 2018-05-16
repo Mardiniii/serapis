58b952c503925a5cefb5eb2019d31841fc88926a
mardiniii <sezama03@hotmail.com>
2018-05-15 18:28:44 -0600

    Remove temporary repo storage for users - Using Postgres

--- api/common/repo.go
+++ api/common/repo.go
@@ -1,33 +1,13 @@
 package common
 
 import (
-	"time"
-
 	db "github.com/Mardiniii/serapis/common/database"
 	"github.com/Mardiniii/serapis/common/models"
 )
 
-// Users collection
-type Users []models.User
-
-var currentID int
-
-// UsersRepo is a temporary users storage
-var UsersRepo Users
-
-func uniqueEmail(email string) bool {
-	for _, u := range UsersRepo {
-		if u.Email == email {
-			return false
-		}
-	}
-
-	return true
-}
-
 // RepoSeedData gives some initial data
 func RepoSeedData() {
-	seeds := Users{
+	seeds := []models.User{
 		models.User{Email: "email1@correo.com"},
 		models.User{Email: "email2@correo.com"},
 		models.User{Email: "email3@correo.com"},
@@ -50,9 +30,6 @@ func RepoFindUserByEmail(email string) (u models.User, err error) {
 func RepoCreateUser(u models.User) (models.User, error) {
 	var err error
 
-	currentID++
-	u.ID = currentID
-	u.CreatedAt = time.Now()
 	u.APIKey = GenerateAPIKey(u)
 	err = db.Connection().CreateUser(&u)
 	return u, err
@@ -61,5 +38,11 @@ func RepoCreateUser(u models.User) (models.User, error) {
 // RepoDestroyUser removes a user
 func RepoDestroyUser(id int) (deleted bool, err error) {
 	err = db.Connection().DeleteUser(id)
+	return
+}
+
+// RepoUsers returns all the users
+func RepoUsers() (users []models.User) {
+	users, _ = db.Connection().GetUsers()
 	return
 }
